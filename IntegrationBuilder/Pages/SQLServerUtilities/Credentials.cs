e19511cd41df4c37315dcce06217c9a0a0388b1b
Christos Karathanasis <christoskarathanasisac@gmail.com>
2023-11-04 10:45:00 +0200

    Step 1 tab and functionality added to the UseVanna Page.

---
+++ IntegrationBuilder/Pages/SQLServerUtilities/Credentials.cs
@@ -0,0 +1,12 @@
+﻿namespace IntegrationBuilder.Pages.SQLServerUtilities
+{
+    public class Credentials
+    {
+        //For Windows Authentication
+        private string server;
+        private string database;
+
+        public string Server { get => server; set => server = value; }
+        public string Database { get => database; set => database = value; }
+    }
+}
---
+++ IntegrationBuilder/Pages/SQLServerUtilities/SQLServerLib.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace IntegrationBuilder.Pages.SQLServerUtilities
+{
+    public static class SQLServerLib
+    {
+        public static bool CheckConnection(string server,string db,out string error)
+        {
+            error = "";
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = server;
+                builder.InitialCatalog = db;
+                //For Windows Authentication
+                builder.IntegratedSecurity = true;
+                builder.ConnectTimeout = 0;
+
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+            }
+            catch (SqlException e)
+            {
+                error = $"Connect to database error. Error message: {e.ToString()}";
+                return false;
+            }
+            return true;
+        }
+    }
+}
